000dedec83e6d52a78b8b05805db3fa9c726ba97
khovan <minhpnq1807@gmail.com>
2025-01-20 01:21:42 +0700

    feat: re-structure feat: make great style

--- Workshop7/src/java/controller/AuthServlet.java
+++ Workshop7/src/java/controller/AuthServlet.java
@@ -4,10 +4,7 @@ import CRUD.DAO;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.*;
 import java.sql.SQLException;
 import model.User;
 
@@ -29,19 +26,13 @@ public class AuthServlet extends HttpServlet {
         String password = request.getParameter("password");
         try {
             User user = authDAO.select(userName, password);
-            if(user!=null){
+            if (user != null) {
             }
             HttpSession session = request.getSession();
-            session.setAttribute("login", true);            
+            session.setAttribute("login", true);
             response.sendRedirect("../index.jsp");
         } catch (SQLException e) {
             response.sendRedirect("login.jsp?error=Authenticate+failed");
         }
     }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }
-
 }
